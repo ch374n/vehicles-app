f10f170da84d197c054516615d91cfaeaea2e02e
ch374n <chetannimbalkar46@gmail.com>
2024-06-27 12:06:08 +0530

    fix: resolved a bug

--- internal/models/manufacturer.go
+++ internal/models/manufacturer.go
@@ -1,14 +1,14 @@
 package models
 
 type VehicleType struct {
-	IsPrimary bool   `json:"IsPrimary" bson:"isPrimary"`
+	IsPrimary bool   `json:"IsPrimary" bson:"isprimary"`
 	Name      string `json:"Name" bson:"name"`
 }
 
 type Manufacturer struct {
 	Country       string        `json:"Country" bson:"country"`
-	MfrCommonName string        `json:"Mfr_CommonName" bson:"mfrCommonName"`
-	MfrID         int           `json:"Mfr_ID" bson:"mfrID"`
-	MfrName       string        `json:"Mfr_Name" bson:"mfrName"`
-	VehicleTypes  []VehicleType `json:"VehicleTypes" bson:"vehicleTypes"`
+	MfrCommonName string        `json:"Mfr_CommonName" bson:"mfrcommonname"`
+	MfrID         int           `json:"Mfr_ID" bson:"mfrid"`
+	MfrName       string        `json:"Mfr_Name" bson:"mfrname"`
+	VehicleTypes  []VehicleType `json:"VehicleTypes" bson:"vehicletypes"`
 }
